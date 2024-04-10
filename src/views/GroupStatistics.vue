4270727c94a13ba6d3d3273723053b4deee3bbdd
1Secand1 <boevaldik@gmail.com>
2024-04-10 18:04:05 +0300

    добавлено отображение студентов относящихся к группу

--- src/views/GroupStatistics.vue
+++ src/views/GroupStatistics.vue
@@ -2,15 +2,43 @@
   Группа
   {{ groupCode }}
   <router-link to="/userGroups">Назад</router-link>
+
+  <DataTable
+    :value="studentList"
+    stripedRows
+  >
+    <Column
+      field="surname"
+      header="Фамилия"
+    />
+    <Column
+      field="name"
+      header="Имя"
+    />
+    <Column
+      field="patronymic"
+      header="Отчество"
+    />
+
+  </DataTable>
 </template>
 
 <script setup>
-import { ref } from "vue"
+import { onMounted, ref } from "vue"
 import { useRoute } from "vue-router"
+import { getStudents } from '../service/getStudents'
+
+import Column from 'primevue/column'
+import DataTable from 'primevue/datatable'
 
 const route = useRoute()
 const groupCode = ref(route.query.codeGroup)
+const studentList = ref([])
 
+onMounted(async () => {
+  studentList.value = await getStudents(groupCode.value)
+  console.log(studentList.value)
+})
 </script>
 
 <style></style>
