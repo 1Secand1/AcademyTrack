65e03f7ba3ce65c7899248f8ceeb738989993857
1Secand1 <boevladik@gmaol.com>
2024-05-01 17:56:50 +0300

    delete getTeachersGroups

--- src/service/getTeachersGroups.js
+++
@@ -1,6 +0,0 @@
-import { useFetch } from './useFetch'
-const { VITE_BASE_API_URL } = import.meta.env
-
-export function getTeachersGroups() {
-	return useFetch(`${VITE_BASE_API_URL}/studentUserGroups`)
-}
