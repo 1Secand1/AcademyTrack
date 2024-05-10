71fd6be9fda0d37b93241d04f0e1f36dbeaa09da
1Secand1 <boevladik@gmail.com>
2024-05-10 13:04:53 +0300

    дикомпозирован компонент GroupStatisticsPage, таблица была вынес в компонент GroupStatisticsPageAttendanceTable

--- src/views/GroupStatisticsPage.vue
+++ src/views/GroupStatisticsPage.vue
@@ -2,7 +2,7 @@
   <div class="heder">
     <div class="row">
       Группа {{ groupCode }}
-      <router-link to="/userGroups">Вернутся</router-link>
+      <router-link to="/userGroups">Вернуться</router-link>
     </div>
 
     <div class="row">
@@ -20,83 +20,42 @@
     :style="{ width: '30rem' }"
   >
     <attendanceListSelectionForm
-      :list='studentNames'
-      :reportableDates='allLessonDates'
-      @change='updateLessonAttendanceReport'
+      :list="studentNames"
+      :reportableDates="allLessonDates"
+      @change="updateLessonAttendanceReport"
     />
   </Dialog>
 
-  <DataTable
-    editMode="cell"
+  <AttendanceDataTable
+    :lessonPlan="lessonPlan"
+    :lessonAttendanceReport="lessonAttendanceReport"
     @cell-edit-complete="onCellEditComplete"
-    :value="studentAttendanceDetails"
-    stripedRows
-    showGridlines
-  >
-    <ColumnGroup type="header">
-      <Row>
-        <Column
-          header="Фио"
-          :rowspan="2"
-          :colspan="1"
-        />
-        <Column
-          v-for="(value, key) in lessonsByMonth"
-          :key='key'
-          :header="key"
-          :colspan="value"
-        />
-      </Row>
-      <Row>
-        <Column
-          v-for='lesson in allLesson'
-          :key='lesson'
-          :header="lesson"
-        />
-      </Row>
-    </ColumnGroup>
-    <Column field="fullName" />
-    <Column
-      #body="slotProps"
-      v-for='value in allLessonDates'
-      :key='value'
-      :field="value"
-    >
-      {{ slotProps.data[value] || "" }}
-    </Column>
-  </DataTable>
+  />
 </template>
 
 <script setup>
-import { computed, onMounted, ref } from "vue"
-import { useRoute } from "vue-router"
-
+import attendanceListSelectionForm from '@components/AttendanceListSelectionForm.vue'
+import AttendanceDataTable from '@components/GroupStatisticsPage/GroupStatisticsPageAttendanceTable.vue'
+import { getLessonAttendanceReport, getLessonPlan } from '@service/apiFunctions'
 import Button from 'primevue/button'
-import Column from 'primevue/column'
-import ColumnGroup from 'primevue/columngroup'
-import DataTable from 'primevue/datatable'
 import Dialog from 'primevue/dialog'
-import Row from 'primevue/row'
-
-
-import { getLessonAttendanceReport, getLessonPlan } from "@service/apiFunctions"
-
-import attendanceListSelectionForm from '@components/attendanceListSelectionForm.vue'
+import { computed, onMounted, ref } from 'vue'
+import { useRoute } from 'vue-router'
 
 const route = useRoute()
-const groupCode = ref(route.query.codeGroup ?? "Неопределенно")
+const groupCode = ref(route.query.codeGroup ?? 'Неопределенно')
 
 const visible = ref(false)
-
 const lessonAttendanceReport = ref({})
 const lessonPlan = ref([])
-
 const studentNames = ref([])
 
-const lessonsByMonth = computed(() => sortLessonsByMonth(lessonPlan.value.lessonsAttendance ?? []))
-const allLesson = computed(() => srtAllClasses(lessonPlan.value.lessonsAttendance ?? []))
-const studentAttendanceDetails = computed(() => mergeStudentAndAttendance(lessonAttendanceReport.value.students ?? []))
-const allLessonDates = computed(() => getColumnFields(lessonPlan.value.lessonsAttendance ?? []))
+const allLessonDates = computed(() => {
+  if (!Array.isArray(lessonPlan.value.lessonsAttendance)) {
+    return []
+  }
+  return lessonPlan.value.lessonsAttendance.map(({ date }) => date)
+})
 
 onMounted(async () => {
   lessonPlan.value = await getLessonPlan()
@@ -104,59 +63,13 @@ onMounted(async () => {
   studentNames.value = getListOfStudents(lessonAttendanceReport.value.students)
 })
 
-
 function onCellEditComplete(event) {
-  console.log(event)
-}
-
-function sortLessonsByMonth(lessons) {
-  if (!Array.isArray(lessons)) {
-    console.warn("lessons in not Array")
-    return []
-  }
-
-  return lessons.reduce((acc, { date }) => {
-    const month = new Date(date).toLocaleString('ru', { month: 'long' })
-    acc[month] = (acc[month] || 0) + 1
-    return acc
-  }, {})
-}
-
-function srtAllClasses(arr) {
-  if (!Array.isArray(arr)) {
-    console.error("The value passed must be an Array")
-    return []
-  }
-
-  return arr.map((lesson) => {
-    const { date } = lesson
-    const dayLesson = date.split('-')[2]
-    return dayLesson
-  })
-}
-
-function mergeStudentAndAttendance(arr) {
-  if (!Array.isArray(arr)) {
-    console.error("The value passed must be an Array")
-    return []
-  }
-
-  return arr.map(({ fullName, attendance }) => {
-    return { fullName, ...attendance }
-  })
-}
-
-function getColumnFields(lessons) {
-  if (!Array.isArray(lessons)) {
-    console.error("The value passed must be an Array")
-    return []
-  }
-  return lessons.map(({ date }) => date)
+  console.log('Cell edit completed:', event)
 }
 
 function getListOfStudents(studentsArr) {
   if (!Array.isArray(studentsArr)) {
-    console.error("The value passed must be an Array")
+    console.error('The value passed must be an Array')
     return []
   }
 
@@ -178,15 +91,9 @@ function updateLessonAttendanceReport({ attendanceList, date }) {
     student.attendance[date] = attendanceStatus
   })
 }
-
-
 </script>
 
 <style scoped>
-.your-style-here {
-  justify-content: center;
-}
-
 .row {
   display: flex;
 }
