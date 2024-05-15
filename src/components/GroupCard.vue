1f88aba11fbf73529f24964dc166643e53d755dd
1Secand1 <boevladik@gmail.com>
2024-05-15 19:38:15 +0300

    на страницу поиска групп добавлены карточки групп

---
+++ src/components/GroupCard.vue
@@ -0,0 +1,39 @@
+<template>
+	<div class="group-card">
+		<div class="group-card__avatar" />
+		<div class="">
+			<h5 class="group-card__title">
+				ИСП-215
+			</h5>
+			<p class="group-card__text">
+				Студентов: 25
+			</p>
+		</div>
+	</div>
+</template>
+
+<style>
+.group-card__avatar {
+	width: 30px;
+	height: 30px;
+	border-radius: 100%;
+	background: #797878;
+}
+
+.group-card {
+	display: flex;
+	align-items: center;
+
+	min-width: 200px;
+
+	border-radius: 5px;
+	background: white;
+
+	padding: 5px 10px;
+	gap: 10px;
+}
+
+.group-card__title {}
+
+.group-card__text {}
+</style>
--- src/views/GroupSelectionPage.vue
+++ src/views/GroupSelectionPage.vue
@@ -1,3 +1,24 @@
 <template>
-	Выберите группу
+	<input type="text">
+	<section class="group-cards">
+		<GroupCard
+			v-for=" i in 25"
+			:key='i'
+		/>
+	</section>
 </template>
+
+<script setup>
+import GroupCard from '@components/GroupCard.vue'
+</script>
+
+<style scoped>
+.group-cards {
+	display: flex;
+	flex-wrap: wrap;
+	justify-content: space-between;
+
+	gap: 20px;
+	margin-top: 25px;
+}
+</style>
