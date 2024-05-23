f5cfb2dd4cb5128c2a8007c675c9d29383a9865f
1Secand1 <boevladik@gmail.com>
2024-05-23 14:49:48 +0300

    функция  useFetch переименована в fetchRequest

--- src/service/serverManager.js
+++ src/service/serverManager.js
@@ -1,28 +1,28 @@
-import { useFetch } from './useFetch';
+import { fetchRequest } from './fetchRequest'
 
-const { VITE_BASE_API_URL } = import.meta.env;
+const { VITE_BASE_API_URL } = import.meta.env
 
 export class ServerManager {
 	constructor(baseUrl, path) {
-		this.baseUrl = baseUrl;
-		this.path = path;
-		this.url = `${this.baseUrl}/${this.path}`;
+		this.baseUrl = baseUrl
+		this.path = path
+		this.url = `${this.baseUrl}/${this.path}`
 	}
 
 	add(studentData) {
-		return useFetch(this.url, 'POST', studentData);
+		return fetchRequest(this.url, 'POST', studentData)
 	}
 
 	get() {
-		return useFetch(this.url);
+		return fetchRequest(this.url)
 	}
 
-	delete() {}
+	delete() { }
 }
 
 export function createServerManager(path) {
 	if (!VITE_BASE_API_URL) {
-		throw new Error('VITE_BASE_API_URL is not defined');
+		throw new Error('VITE_BASE_API_URL is not defined')
 	}
-	return new ServerManager(VITE_BASE_API_URL, path);
+	return new ServerManager(VITE_BASE_API_URL, path)
 }
