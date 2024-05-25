b6e432e32654556d5e775088d745321289aa0642
1Secand1 <boevladik@gmail.com>
2024-05-25 17:30:04 +0300

    feat:  карточка  урока  вынесена в отдельный компонент

---
+++ src/components/GroupProfile/GroupProfileDayScheduleCard/GroupProfileDayScheduleCard.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="card">
+    <slot />
+  </div>
+</template>
+
+<style scoped>
+.card {
+	display: flex;
+	width: 380px;
+	height: 75px;
+
+	padding: 10px;
+	align-items: center;
+	border-radius: 5px;
+	border: solid 1px #2196F3;
+}
+</style>
