949f166d01025ee663fccb3409338f6392aa6a7d
1Secand1 <boevladik@gmail.com>
2024-05-25 19:59:17 +0300

    feat: добавлена номерованная карточка

---
+++ src/components/GroupProfile/GroupProfileDayScheduleCard/GroupProfileDayScheduleCardNumbered.vue
@@ -0,0 +1,36 @@
+<template>
+  <GroupProfileDayScheduleCard>
+    <span class="card__lesson-number">
+      {{ number }}
+    </span>
+    <div class="card__info-box">
+      <slot />
+    </div>
+  </GroupProfileDayScheduleCard>
+</template>
+
+<script setup>
+  import GroupProfileDayScheduleCard from '@components/GroupProfile/GroupProfileDayScheduleCard/GroupProfileDayScheduleCard.vue';
+
+  defineProps(['number']);
+</script>
+
+<style scoped>
+.card__lesson-number {
+	display: grid;
+	place-items: center;
+
+	min-width: 35px;
+	height: 35px;
+
+	border-radius: 100%;
+	background: #2196F3;
+	color: rgb(255, 255, 255);
+}
+
+.card__info-box {
+	display: grid;
+	margin-left: 10px;
+	width: 100%;
+}
+</style>
