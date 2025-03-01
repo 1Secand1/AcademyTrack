d12f44179365a3b2bb5157501b61c112c877a068
boevvv <boevvv@eastmining.ru>
2025-03-01 09:08:07 +0300

    feat: disable authorization

--- src/router/index.js
+++ src/router/index.js
@@ -9,17 +9,17 @@ const router = createRouter({
 
 const publiclyRoutes = ['authorization'];
 
-router.beforeEach(to => {
-  const isUserAuthorized = !!getCookie('token');
-  const isRoutePublic = publiclyRoutes.includes(to.name);
-
- if (isUserAuthorized && isRoutePublic) {
-		router.push({ name:'userGroups' });
- }
-
- if (!isUserAuthorized && !isRoutePublic) {
-	router.push({ name:'authorization' });
- }
-});
+// router.beforeEach(to => {
+//   const isUserAuthorized = !!getCookie('token');
+//   const isRoutePublic = publiclyRoutes.includes(to.name);
+//
+//  if (isUserAuthorized && isRoutePublic) {
+// 		router.push({ name:'userGroups' });
+//  }
+//
+//  if (!isUserAuthorized && !isRoutePublic) {
+// 	router.push({ name:'authorization' });
+//  }
+// });
 
 export default router;
